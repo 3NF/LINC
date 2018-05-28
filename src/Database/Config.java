ff9b7d70b8e130227580ee9f628a1e302cbe1757
Luka Tchumburidze <lukatchumburidze@gmail.com>
2018-05-28 13:17:56 +0400

    Added database credentials in the Config

--- src/Database/Config.java
+++ src/Database/Config.java
@@ -1,4 +1,8 @@
 package Database;
 
 public class Config {
+    public static final String MYSQL_USERNAME = "LINCAdministrator";
+    public static final String MYSQL_PASSWORD = "ThisPassIsHard58943012";
+    public static final String MYSQL_DATABASE_SERVER = "jdbc:mysql://localhost:3306";
+    public static final String MYSQL_DATABASE_NAME = "LINCDB";
 }
