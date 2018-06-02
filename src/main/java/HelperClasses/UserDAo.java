114a44fe1fd62b535bc2be23a5bffe237050f3e1
bakurits <bakuricucxashvili@gmail.com>
2018-06-03 02:48:36 +0400

    added google authorization

--- src/main/java/HelperClasses/UserDAo.java
+++ src/main/java/HelperClasses/UserDAo.java
@@ -9,57 +9,73 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserDAo {
-    /*
-        connection pull manager
-     */
-    private final BasicDataSource connectionPool;
+	/*
+		connection pull manager
+	 */
+	private final BasicDataSource connectionPool;
 
-    /**
-     * contructor of StudentDAo class
-     *
-     * @param connectionPool
-     */
-    public UserDAo(BasicDataSource connectionPool) {
-        this.connectionPool = connectionPool;
-    }
+	/**
+	 * contructor of StudentDAo class
+	 *
+	 * @param connectionPool // TODO: 6/3/18
+	 */
+	public UserDAo(BasicDataSource connectionPool) {
+		this.connectionPool = connectionPool;
+	}
 
-    public static void main(String args[]) {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl(Config.MYSQL_DATABASE_SERVER);
-        ds.setUsername(Config.MYSQL_USERNAME);
-        ds.setPassword(Config.MYSQL_PASSWORD);
-        UserDAo user = new UserDAo(ds);
-        User u = user.getUser("prochi", "traki");
-        System.out.println(u.getEmail());
-    }
+	public static void main(String args[]) {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl(Config.MYSQL_DATABASE_SERVER);
+		ds.setUsername(Config.MYSQL_USERNAME);
+		ds.setPassword(Config.MYSQL_PASSWORD);
+		UserDAo user = new UserDAo(ds);
+		User u = user.getUser("prochi", "traki");
+		System.out.println(u.getEmail());
+	}
 
-    /**
-     * @param email
-     * @param password
-     * @return
-     */
-    public User getUser(String email, String password) {
-        Connection connection = null;
-        try {
-            connection = connectionPool.getConnection();
-        } catch (SQLException e) {
-            System.err.println("error in getting connection");
-        }
-        try {
-            Statement statement = connection.createStatement();
-            String query = "Select * FROM " + Config.MYSQL_DATABASE_NAME + "." + "users WHERE email=" + "'" + email + "'" + " AND password=" + "'" + password + "'";
-            ResultSet result = statement.executeQuery(query);
-            while (result.next()) {
-                String firstName = result.getString("firstname");
-                String lastName = result.getString("secondname");
-                String userRole = result.getString("userrole");
-                return (User) new User(email,password,firstName,lastName, User.Role.valueOf(userRole));
-            }
-            connection.close();
-        } catch (SQLException e) {
-            System.err.println("exception in creation statement");
-        }
-        return null;
-    }
+	/**
+	 * // TODO: 6/3/18
+	 *
+	 * @param email
+	 * @param password
+	 *
+	 * @return
+	 */
+	public User getUser(String email, String password) {
+		Connection connection = null;
+		try {
+			connection = connectionPool.getConnection();
+		} catch (SQLException e) {
+			System.err.println("error in getting connection");
+		}
+
+		if (connection == null) return null;
+
+		try {
+			Statement statement = connection.createStatement();
+			String query = "Select * FROM " + Config.MYSQL_DATABASE_NAME + "." + "users WHERE email=" + "'" + email + "'" + " AND password=" + "'" + password + "'";
+			ResultSet result = statement.executeQuery(query);
+			while (result.next()) {
+				String firstName = result.getString("firstname");
+				String lastName = result.getString("secondname");
+				String userRole = result.getString("userrole");
+				connection.close();
+				return new User(email, password, firstName, lastName, User.Role.valueOf(userRole));
+			}
+		} catch (SQLException e) {
+			System.err.println("exception in creation statement");
+		}
+		return null;
+	}
+
+	/**
+	 * @param email
+	 *
+	 * @return
+	 */
+	public User getUserByEmail(String email) {
+		// TODO: 6/3/18 implement this method
+		return null;
+	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,3 @@
-
-
 <html>
 
 <head>
@@ -7,25 +5,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <%-- Ajax --%>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+    <%-- Bootstrap --%>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="Styles/assets/css/bootstrap.css" rel="stylesheet">
     <link href="Styles/assets/css/font-awesome.css" rel="stylesheet">
     <link href="Styles/assets/css/docs.css" rel="stylesheet">
     <link href="Styles/bootstrap-social.css" rel="stylesheet">
 
-
+    <%-- My css --%>
     <link rel="stylesheet" href="Styles/style.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <%-- Google's authorization --%>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id"
+          content="108555998588-rcq9m8lel3d81vk93othgsg2tolfk9b9.apps.googleusercontent.com">
+
 </head>
 
 <body>
 <div class="container">
 
     <form id="form_login" action="/LoginCheck" method="post">
-        <div hidden id = "wrong-pass" class="alert alert-danger">
+        <div hidden id="wrong-pass" class="alert alert-danger">
             Incorrect email or password.
         </div>
         <div class="input-group">
@@ -39,8 +45,7 @@
         <input type="submit" id="login-page-login-button" class="btn btn-primary" value="Login">
         <br>
         <br>
-        <a class="btn btn-block btn-social btn-lg btn-google" onclick="loginWithGoogle()"><i class="fa fa-google"></i>Sign
-            in with Google</a>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
     </form>
 </div>
 </body>
@@ -49,6 +54,24 @@
     let wrongPass = <%= (request.getAttribute("wrongPassword") != null) %>;
     if (wrongPass) {
         $('#wrong-pass').show();
+    }
+
+    function onSignIn(googleUser) {
+        let id_token = googleUser.getAuthResponse().id_token;
+        $.post("/GoogleLogin", {id_token: id_token}, function (data) {
+            console.log(data);
+            switch (data) {
+                case "success": {
+                    window.location.href = "dashboard.jsp";
+                    break;
+                }
+                default: {
+                    gapi.auth2.getAuthInstance().disconnect();
+                    location.reload();
+                    break;
+                }
+            }
+        });
     }
 </script>
 
