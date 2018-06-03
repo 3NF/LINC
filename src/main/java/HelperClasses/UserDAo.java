14f1b31f4dd24bc0bad6b89367bdb93fc9e187b6
3NFBAGDU <gbagh16@freeuni.edu.ge>
2018-06-03 12:44:09 +0400

    userDAo changes

--- src/main/java/HelperClasses/UserDAo.java
+++ src/main/java/HelperClasses/UserDAo.java
@@ -1,12 +1,12 @@
 package HelperClasses;
 
-import Database.Config;
-import org.apache.commons.dbcp2.BasicDataSource;
+        import Database.Config;
+        import org.apache.commons.dbcp2.BasicDataSource;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+        import java.sql.Connection;
+        import java.sql.ResultSet;
+        import java.sql.SQLException;
+        import java.sql.Statement;
 
 public class UserDAo {
     /*
---
+++ src/test/java/Servlets/GoogleLoginTest.java
@@ -0,0 +1,35 @@
+package Servlets;
+
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.atLeast;
+
+public class GoogleLoginTest {
+    @Test
+    public void testServlet() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        when(request.getParameter("UserDAo")).thenReturn("gbagh16@freeuni.edu.ge");
+        when(request.getParameter("password")).thenReturn("secret");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+        new LoginCheck().doPost(request, response);
+
+        verify(request, atLeast(1)).getParameter("username"); // only if you want to verify username was called...
+        writer.flush(); // it may not have been flushed yet...
+        assertTrue(stringWriter.toString().contains("My expected string"));
+    }
+}
