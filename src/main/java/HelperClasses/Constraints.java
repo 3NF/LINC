08813e2714c56fb8dc5a614e105e6ee819165107
Giorgi <zauriMiqeladze123@gmail.com>
2018-06-04 21:00:40 +0400

    Removed useless constant

--- src/main/java/HelperClasses/Constraints.java
+++ src/main/java/HelperClasses/Constraints.java
@@ -7,10 +7,4 @@ public class Constraints
      * Timeout for each user session in minutes. When timeout expires, user logs out automatically.
      */
     public static final int SESSION_TIMEOUT = 5 * 60;
-
-
-    /**
-     * Max inactivity time for logged user in minutes. When timeout expires, user logs out outomatically.
-     */
-    public static final int MAX_INACTIVE_TIME = 15;
 }
