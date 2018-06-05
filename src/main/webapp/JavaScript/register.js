f447fa2c91b8b7a848868cbbecdee48df108a080
Giorgi <zauriMiqeladze123@gmail.com>
2018-06-05 05:15:39 +0400

    register page front-end complete

--- src/main/webapp/JavaScript/register.js
+++ src/main/webapp/JavaScript/register.js
@@ -1,19 +1,58 @@
 
-function checkForm(loginForm) {
-
-        if(
-            loginForm.email.value === "" || loginForm.firstName.value === "" ||
-            loginForm.lastName.value === "" || loginForm.password === "" ||
-            loginForm.confirmEmail.value === "" || loginForm.confirmPassword === ""
-          )
+function checkForm(loginForm)
+{
+    document.alertBox = document.getElementById("alertBox");
+    document.alertBox.innerHTML = "";
+    let result = true;
+    let fields = loginForm.querySelector(".input-group").querySelectorAll("input");
+    fields.forEach(function (value, index, listObj) {
+        if(value.value === "")
         {
-            let div = document.getElementById("errorAlert");
-            div.innerHTML ="Please Fill All Fields!";
-            div.removeAttribute("hidden");
-            loginForm.insertBefore(div,loginForm.childNodes[2]);
-            return false;
-        }
+            result = false;
+            value.style.borderColor = "red";
+        }else value.style.borderColor = "";
+    });
+
+    if(result === false) displayError("Please Fill All Fields");
 
 
+    if(loginForm.password.value.length < 8)
+    {
+        displayError("Password Length Should Be At Least 8!");
+        result = false;
+    }
+
+    if(!checkInputs(loginForm.password,loginForm.confirmPassword))
+    {
+        displayError("Passwords Not Match!");
+        result = false;
+    }
+
+    if(!checkInputs(loginForm.email,loginForm.confirmEmail))
+    {
+        displayError("Emails Not Match");
+        result = false;
+    }
+
+    return result
+}
+
+
+
+function checkInputs(input1, input2)
+{
+    if(input1.value !== input2.value)
+    {
+        input1.style.borderColor  = "red";
+        input2.style.borderColor = "red";
         return false;
+    }
+
+    return true;
+}
+
+
+function displayError(text)
+{
+    document.alertBox.innerHTML += "<li class='text text-danger' style='width: 100%; margin-bottom: 5px'>"  + text +  "</li>";
 }
