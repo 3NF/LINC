78bd3a1c73653c0463033f6432e6c6eff4572c24
Giorgi <zauriMiqeladze123@gmail.com>
2018-06-07 01:40:38 +0400

    Added room class

---
+++ src/main/java/Core/Room.java
@@ -0,0 +1,12 @@
+package Core;
+
+import java.util.ArrayList;
+
+public class Room
+{
+    String title;
+    String name;
+    String coverImage;
+    String backgroundImage;
+    ArrayList<User> users;
+}
