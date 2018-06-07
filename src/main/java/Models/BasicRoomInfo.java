4d6d29cc4873211199d84df5361b15f488c82c7b
Giorgi <zauriMiqeladze123@gmail.com>
2018-06-07 10:13:40 +0400

    Created

---
+++ src/main/java/Models/BasicRoomInfo.java
@@ -0,0 +1,32 @@
+package Models;
+
+
+/**
+ * represents classroom, but only contains basic information.
+ * Mostly used as field of user object.
+ */
+public class BasicRoomInfo
+{
+    private String name;
+    private String title;
+    private String coverImagePath;
+
+
+    public BasicRoomInfo(String name, String title, String coverImagePath) {
+        this.name = name;
+        this.title = title;
+        this.coverImagePath = coverImagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCoverImagePath() {
+        return coverImagePath;
+    }
+}
