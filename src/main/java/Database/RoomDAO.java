5fa18041e2c4a29a27bb5c99e8aa34b0944adad0
Giorgi <zauriMiqeladze123@gmail.com>
2018-06-07 08:24:19 +0400

    Created File

---
+++ src/main/java/Database/RoomDAO.java
@@ -0,0 +1,26 @@
+package Database;
+
+import Core.Room;
+
+public class RoomDAO
+{
+
+    /**
+     * @param room Unique name of room
+     * @return corresponding Room object
+     */
+    public Room getRoomByName(String room)
+    {
+        return null;
+    }
+
+
+    /**
+     * @param userID unique user id obtained from google
+     * @return corresponding Room object
+     */
+    public Room getRoomsByUser(String userID)
+    {
+        return null;
+    }
+}
