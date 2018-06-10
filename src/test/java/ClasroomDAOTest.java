e686e83e37356495dc6107afce7f9569b6d84242
https://giorgibaghdavadze@bitbucket.org/thomasmikava/murtsku.git <gbagh16@freeuni.edu.ge>
2018-06-10 16:00:53 +0400

    add clasroomDAOTest  ჯერჯერობით ასეთი წარმომიდგენია კლასრუმდაო მერე ვიმსჯელოთ, მარა ასეთი კოდი არატესტირებადია აბსოლიტურად.

--- src/test/java/ClasroomDAOTest.java
+++ src/test/java/ClasroomDAOTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class ClasroomDAOTest {
@@ -10,6 +12,13 @@ public class ClasroomDAOTest {
 
     @Test
     public void testGetNames(){
-
+        ClasroomDAO clasroom = null;
+        try {
+            clasroom = new ClasroomDAO();
+        } catch (IOException e) {
+            System.err.println("error in classroom creation");
+        }
+        for (String className : clasroom.getClassroomNames()){
+        }
     }
 }
