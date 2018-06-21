069d640e16367ed882a4dce4cc6d9546af1ad144
Luka Tchumburidze <lukatchumburidze@gmail.com>
2018-06-21 14:04:51 +0400

    Changed MYSQL_DATABASE_SERVER constant with correct one.

--- src/main/java/Database/Config.java
+++ src/main/java/Database/Config.java
@@ -3,7 +3,7 @@ package Database;
 public class Config {
     public static final String MYSQL_USERNAME = "a357a4_linc";
     public static final String MYSQL_PASSWORD = "dzliera123";
-    public static final String MYSQL_DATABASE_SERVER = "mysql6002.site4now.net";
+    public static final String MYSQL_DATABASE_SERVER = "jdbc:mysql://mysql6002.site4now.net:3306";
     public static final String MYSQL_DATABASE_NAME = "db_a357a4_linc";
 
     /*
