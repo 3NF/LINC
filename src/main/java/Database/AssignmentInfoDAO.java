17e8d09797feff0f012c25c8b44d777923509955
bakurits <bakuricucxashvili@gmail.com>
2018-06-30 16:05:19 +0400

    sorry gio

--- src/main/java/Database/AssignmentInfoDAO.java
+++ src/main/java/Database/AssignmentInfoDAO.java
@@ -1,5 +1,12 @@
 package Database;
 
+import java.util.Set;
+
 public class AssignmentInfoDAO {
+
+
+    public Set<String> getAssignmentFilesNames(String classID, String assignmentID) {
+        return null;
+    }
 
 }
---
+++ src/test/java/Servlets/AddAssignmentServletTest.java
@@ -0,0 +1,62 @@
+package Servlets;
+
+import Database.AssignmentInfoDAO;
+import Database.GAPIManager;
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static Data.Constraints.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AddAssignmentServletTest {
+
+    @Test
+    public void doPostInvalidUser() throws IOException, ServletException, JSONException {
+
+        AddAssignmentServlet servlet = new AddAssignmentServlet();
+
+        AssignmentInfoDAO dao = mock(AssignmentInfoDAO.class);
+        GAPIManager gapiManager = mock(GAPIManager.class);
+        Set<String> st = new HashSet<String>();
+        st.addAll(Arrays.asList(new String[] {"sample.h", "sample.cpp", "main.c"}));
+        when(dao.getAssignmentFilesNames("-", "-")).thenReturn(st);
+        when(gapiManager.getUser("-")).thenReturn(null);
+        when(gapiManager.isInRoom("-", "-")).thenReturn(true);
+
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        when(req.getParameter(USER_ID_TOKEN)).thenReturn("-");
+        when(req.getParameter(ASSIGNMENT)).thenReturn("-");
+        when(req.getParameter(ROOM_ID)).thenReturn("-");
+        when(req.getParameter(ASSIGNMENT_ID)).thenReturn("-");
+        
+        ServletContext context = mock(ServletContext.class);
+        when(context.getAttribute(ASSIGNMENT_INFO_DAO)).thenReturn(dao);
+        when(context.getAttribute(GAPI_MANAGER)).thenReturn(gapiManager);
+        when(req.getServletContext()).thenReturn(context);
+
+
+        HttpServletResponse res = mock(HttpServletResponse.class);
+        StringWriter writer = new StringWriter();
+        when(res.getWriter()).thenReturn(new PrintWriter(writer));
+
+        servlet.doPost(req, res);
+
+        String actual = "{\"error-message\":\"InvalidUser\"}";
+        JSONAssert.assertEquals(writer.toString(), actual, JSONCompareMode.LENIENT);
+    }
+}
