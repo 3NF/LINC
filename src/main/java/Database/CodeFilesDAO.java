fb1e426a0fb92bc5e4d46f9a497a858f560df240
https://giorgibaghdavadze@bitbucket.org/thomasmikava/murtsku.git <gbagh16@freeuni.edu.ge>
2018-06-30 16:00:08 +0400

    Merge branch 'master' of https://github.com/3NF/LINC
    # Conflicts:
    #	src/main/java/Database/CodeFilesDAO.java

--- src/main/java/Database/CodeFilesDAO.java
+++ src/main/java/Database/CodeFilesDAO.java
@@ -2,6 +2,7 @@ package Database;
 
 import Data.Suggestion;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,19 +18,19 @@ public class CodeFilesDAO {
         this.connectionPool = connectionPool;
     }
 
-    public CodeFile getFilesContent(String userId,String codeFileId) throws SQLException {
+    public CodeFile getFilesContent(String userId, String codeFileId) throws SQLException {
         Connection connection = connectionPool.getConnection();
 
         String query = "SELECT assignment_files.lang,assignment_files.name,code_files.content FROM assignment_files inner join " +
                 "code_files on assignment_files.id=code_files.filesID where code_files.userID=? AND code_files.filesID=?";
         PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1,userId);
-        statement.setString(2,codeFileId);
+        statement.setString(1, userId);
+        statement.setString(2, codeFileId);
         ResultSet result = statement.executeQuery();
         String codeContent = "";
         String fileName = "";
         String codeLang = "";
-        if (result.next()){
+        if (result.next()) {
             codeContent = result.getString("content");
             fileName = result.getString("name");
             codeLang = result.getString("lang");
@@ -37,24 +38,24 @@ public class CodeFilesDAO {
 
         query = "SELECT * FROM " + Config.MYSQL_DATABASE_NAME + ".suggestions WHERE fileId=?";
         statement = connection.prepareStatement(query);
-        statement.setString(1,codeFileId);
+        statement.setString(1, codeFileId);
         //System.err.println(query);
         result = statement.executeQuery();
         List<Suggestion> suggestions = new ArrayList<>();
-        while (result.next()){
+        while (result.next()) {
             String suggestionId = result.getString("suggestionID");
             String uId = result.getString("uid");
             String text = result.getString("text");
             String type = result.getString("type");
             Date date = new Date(result.getDate("time").getTime());
             Suggestion.SuggestionType suggestionType = Suggestion.SuggestionType.valueOf(type);
-            suggestions.add(new Suggestion(suggestionType,uId,"sds",codeFileId,suggestionId,0,0,text,date));
+            suggestions.add(new Suggestion(suggestionType, uId, "sds", codeFileId, suggestionId, 0, 0, text, date));
         }
         connection.close();
-        return new CodeFile(codeContent,codeFileId,fileName,suggestions,codeLang);
+        return new CodeFile(codeContent, codeFileId, fileName, suggestions, codeLang);
     }
 
-    public void tempSave (long userId, long fileID, String content) throws SQLException {
+    public void tempSave(long userId, long fileID, String content) throws SQLException {
         Connection connection = connectionPool.getConnection();
 
         String query = "INSERT INTO code_files (userID, filesID, content) VALUES\n" +
@@ -69,14 +70,13 @@ public class CodeFilesDAO {
     }
 
 
-
     public List<String> getAssignmentCodesNames(String id) throws SQLException {
         String query = "Select id,name FROM assignment_files where id=?";
         Connection connection = connectionPool.getConnection();
         PreparedStatement statement = connection.prepareStatement(query);
-        statement.setString(1,id);
+        statement.setString(1, id);
         ResultSet result = statement.executeQuery();
-        List <String> codeFileNames = new ArrayList<String>();
+        List<String> codeFileNames = new ArrayList<String>();
         return null;
     }
 }
