617885f2473f46d5a263fba582a664d2ca78d98e
Luka Tchumburidze <lukatchumburidze@gmail.com>
2018-07-13 03:56:45 +0400

    Restored suggestion adding functionality

--- src/main/java/Servlets/SuggestionDispatcher.java
+++ src/main/java/Servlets/SuggestionDispatcher.java
@@ -80,7 +80,7 @@ public class SuggestionDispatcher extends HttpServlet
             UserStorage userStorage = (UserStorage) request.getServletContext().getAttribute(USER_STORAGE);
 
             suggestion.RetrieveUsers(user.getUserId(), userStorage);
-            return new GsonBuilder().disableHtmlEscaping().create().toJson(suggestionDAO.addSuggestion(user, codeFileID, type, content, startInd, endInd));
+            return new GsonBuilder().disableHtmlEscaping().create().toJson(suggestion);
         } catch (ClassCastException|IllegalArgumentException e) {
             e.printStackTrace();
             return null;
