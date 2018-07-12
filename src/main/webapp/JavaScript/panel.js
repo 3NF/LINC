0dfc60a1de9c24c2deebb2149f19e8a72369c3a5
https://giorgibaghdavadze@bitbucket.org/thomasmikava/murtsku.git <gbagh16@freeuni.edu.ge>
2018-07-12 13:20:58 +0400

    add confirm window alert, to confirm if teacher really want to download assignment, we are shota's students and bochora's angels.

--- src/main/webapp/JavaScript/panel.js
+++ src/main/webapp/JavaScript/panel.js
@@ -65,20 +65,24 @@ function getAssignment(assignmentId) {
 
 function sendAssignments(assignmentId) {
     const courseID = getParameter("courseID");
-    console.log(courseID);
-    $.ajax({
-        type:'POST',
-        url: '/teacher-dispatcher',
-        data: JSON.stringify(
-            {"assignmentID" : assignmentId,
-            "courseID" : courseID}
-        ),
-        success:function(){
-            alert("Successfully downloaded assignment files.");
-        },
-        error:function(){
-            console.log('Service call failed!');
-        }
-    });
+    if (window.confirm("Do You want to download assignments?")) {
+        console.log(courseID);
+        $.ajax({
+            type: 'POST',
+            url: '/teacher-dispatcher',
+            data: JSON.stringify(
+                {
+                    "assignmentID": assignmentId,
+                    "courseID": courseID
+                }
+            ),
+            success: function () {
+                alert("Successfully downloaded assignment files.");
+            },
+            error: function () {
+                console.log('Service call failed!');
+            }
+        });
+    }
 }
 
