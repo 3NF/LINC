2f63c0e0d2a2d4c7b2e43e8e0c9f61270aeae41d
Luka Tchumburidze <lukatchumburidze@gmail.com>
2018-07-13 15:13:34 +0400

    Minor loading icon update

--- src/main/webapp/JavaScript/dashboard-instructor-controls.js
+++ src/main/webapp/JavaScript/dashboard-instructor-controls.js
@@ -172,6 +172,7 @@ function clearInterval () {
 }
 
 function submitSuggestion () {
+    toggleLoading();
     var suggestionContent = suggestionEditor.parseContent();
     var suggestionType = $("#suggestion-type").html();
 
@@ -201,9 +202,11 @@ function submitSuggestion () {
             console.log(suggestions);
             viewSuggestion(suggestions.length-1);
             placeSuggestion(suggestions.length-1, data);
+            toggleLoading();
         },
         error: function (data, textStatus, jQxhr) {
             suggestionAdditionError(data, textStatus, jQxhr);
+            toggleLoading();
         }});
 }
 
@@ -241,6 +244,7 @@ function suggestionAdditionError () {
 }
 
 function deleteClick () {
+    toggleLoading();
     suggID = activeSuggestionID;
     $.ajax({
         url: "/user/suggestion_dispatcher",
@@ -251,9 +255,11 @@ function deleteClick () {
             removeSuggestion(suggID);
             $("#comment-panel").hide();
             activeSuggestionID = -1;
+            toggleLoading();
         },
         error: function () {
             suggestRemoveError();
+            toggleLoading();
         }});
 }
 
@@ -262,6 +268,7 @@ function suggestRemoveError () {
 }
 
 function updateGrade(selectObj) {
+
     if (window.confirm("Do You want to grade assignments?")) {
         let selectIndex=selectObj.selectedIndex;
         let grade=selectObj.options[selectIndex].text;
