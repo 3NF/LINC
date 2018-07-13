69eefa5c72f51e06f1f2a1eba7ae558335dfbc6b
bakurits <bakuricucxashvili@gmail.com>
2018-07-13 10:50:14 +0400

    local database

--- src/main/java/Database/Config.java
+++ src/main/java/Database/Config.java
@@ -4,8 +4,8 @@ package Database;
  * Stores database config
  */
 public class Config {
-    public static final String MYSQL_USERNAME = "a357a4_linc";
-    public static final String MYSQL_PASSWORD = "dzliera123";
-    public static final String MYSQL_DATABASE_SERVER = "mysql6002.site4now.net";
-    public static final String MYSQL_DATABASE_NAME = "db_a357a4_linc";
+    public static final String MYSQL_USERNAME = "LINCAdministrator";
+    public static final String MYSQL_PASSWORD = "ThisPassIsHard58943012";
+    public static final String MYSQL_DATABASE_SERVER = "jdbc:mysql://localhost:3306/LINCDB?useSSL=false";
+    public static final String MYSQL_DATABASE_NAME = "LINCDB";
 }
